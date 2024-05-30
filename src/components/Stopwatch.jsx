6938683036e8dcccbc102ffd1fcd4470c02d5ffa
madyanes <madyanes.dev@gmail.com>
2024-05-31 00:36:44 +0700

    [FEAT] stop button

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function Stopwatch() {
   const [startTime, setStartTime] = useState(null)
   const [now, setNow] = useState(null)
+  const intervalRef = useRef(null)
 
   function handleStart() {
     // Start counting.
     setStartTime(Date.now())
     setNow(Date.now())
 
-    setInterval(() => {
+    clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
       // Update the current time every 10ms.
       setNow(Date.now())
     }, 10)
+  }
+
+  function handleStop() {
+    clearInterval(intervalRef.current)
   }
 
   let secondsPassed = 0
@@ -23,6 +29,7 @@ export default function Stopwatch() {
   return (
     <>
       <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
       <p>{secondsPassed.toFixed(3)} seconds</p>
     </>
   )
